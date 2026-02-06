412a1d703564715b4c7f0f92daa09c1b271edd85
SanikaMJ2005 <4mh23cs131@gmail.com>
2026-02-06 12:55:40 +0530

    sdf

--- main.py
+++ main.py
@@ -30,30 +30,38 @@ def create_student(student:Student)->StudentResponse:
     students.append(student)
     return student
 
+def get_student_by_roll(roll):   
+    for student in students:
+        if student.Roll_number==roll:
+            return student
 
-def read_student(id:int)->StudentResponse:
-    return StudentResponse(id=id, **student.dict())
+def read_student(roll:str)->StudentResponse:
+    return get_student_by_roll(roll)
 
 
-def update_student(id:int,student:Student)->StudentResponse:
-    return StudentResponse(id=id, **student.dict())
 
-def delete_student(id:int):
-    return StudentResponse(id=id, **student.dict())
+def update_student(roll:str,student:Student)->StudentResponse:
+    return StudentResponse(roll=roll, **student.dict())
 
+def delete_student(roll:str):
+    return StudentResponse(roll=roll, **student.dict())
+
+@app.get("/students")
+def read_students():
+    return students
 
 @app.post("/students")
 def create_student_api(student:Student):
     return create_student(student)
 
-@app.get("/students/{id}")
-def read_student_api(id:int):
-    return read_student(id)
+@app.get("/students/{roll}")
+def read_student_api(roll:str):
+    return read_student(roll)
 
-@app.put("/students/{id}")
-def update_student_api(id:int,student:Student):
-    return update_student(id,student)
+@app.put("/students/{roll}")
+def update_student_api(roll:str,student:Student):
+    return update_student(roll,student)
 
-@app.delete("/students/{id}")
-def delete_student_api(id:int):
-    return delete_student(id)
+@app.delete("/students/{roll}")
+def delete_student_api(roll:str):
+    return delete_student(roll)
